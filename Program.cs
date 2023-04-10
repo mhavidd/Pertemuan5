70fb58ff1faadcb5942a945fea348bc6a9578705
mhavidd <muhammadhavid04@students.amikom.ac.id>
2023-04-11 05:44:53 +0700

    Prak5

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace laprak5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers numbers = new Numbers();
+            numbers.CariNilaiMin(28, 24);
+            numbers.CariNilaiMin(28, 24, 56);
+            numbers.CariNilaiMax(28, 24);
+            numbers.CariNilaiMax(28, 24, 56);
+        }
+    }
+}
